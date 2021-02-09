35a36242fbdd7a2b38415ad802586ad034634968
Rixsimov <rixsimovsunnatilla@gmail.com>
2021-02-09 09:53:21 +0500

    First commit

---
+++ js/swiper-style.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  });
